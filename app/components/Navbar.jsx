b291dda1a67d8803643accfe7534db6d54d24794
okurutjoseph <okurutjoseph97@gmail.com>
2025-02-19 16:43:28 +0300

    commit

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,21 +5,21 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
-        <nav>
-            <a href="">
-                <Image src={assets.logo} className='w-28 cursor-pointer mr-14'/>
+        <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+            <a href="#top">
+                <Image src={assets.logo} alt="" className='w-28 cursor-pointer mr-14'/>
             </a>
 
-            <ul>
-                <li><a href="#top">Home</a></li>
-                <li><a href="#about">About me</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#work">My Work</a></li>
-                <li><a href="#contact">Contact me</a></li>
+            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+                <li><a className='font-Ovo' href="#top">Home</a></li>
+                <li><a className='font-Ovo' href="#about">About me</a></li>
+                <li><a className='font-Ovo' href="#services">Services</a></li>
+                <li><a className='font-Ovo' href="#work">My Work</a></li>
+                <li><a className='font-Ovo' href="#contact">Contact me</a></li>
             </ul>
 
             <div>
-                <a href="#contact">Contact <Image src={assets.arrow_icon} className='w-3'/></a>
+                <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo'>Contact <Image src={assets.arrow_icon} alt="" className='w-3'/></a>
             </div>
         </nav>
     </>
