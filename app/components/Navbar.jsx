5c22d3a10f324562353cec342e928f93a46572fa
okurutjoseph <okurutjoseph97@gmail.com>
2025-02-19 16:57:24 +0300

    header changes

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -5,12 +5,16 @@ import React from 'react'
 const Navbar = () => {
   return (
     <>
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <Image src={assets.header_bg_color} alt='' className='w-full' />
+    </div>
+
         <nav className='w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
             <a href="#top">
                 <Image src={assets.logo} alt="" className='w-28 cursor-pointer mr-14'/>
             </a>
 
-            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3'>
+            <ul className='hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50'>
                 <li><a className='font-Ovo' href="#top">Home</a></li>
                 <li><a className='font-Ovo' href="#about">About me</a></li>
                 <li><a className='font-Ovo' href="#services">Services</a></li>
