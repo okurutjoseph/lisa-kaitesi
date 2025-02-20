42b174e87021ac591340e5bc63d56fa3502f7d7c
okurutjoseph <okurutjoseph97@gmail.com>
2025-02-20 20:10:37 +0300

    contact form added

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,3 +1,5 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const Contact = () => {
@@ -11,7 +13,22 @@ const Contact = () => {
         <p className='text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo'>
             I'd love to hear from you! If you have any questions, comments, or feedback, Please use the form below.</p>
 
-            <form action=""></form>
+            <form className='max-w-2xl mx-auto'>
+              <div className='grid grid-cols-auto gap-6 mt-10 mb-8'>
+                <input type="text" placeholder='Enter your name' required
+                className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'/>
+                <input type="email" placeholder='Enter your email' required
+                className='flex-1 p-3 outline-none border-[0.5px] border-gray-400 rounded-md bg-white'/>
+              </div>
+              <textarea rows='6' placeholder='Enter your message' required
+              className='w-full p-4 outline-none border-[0.5px] border-gray-400 rounded-md bg-white mb-6'></textarea>
+
+              <button type='submit'
+              className='py-3 px-8 w-max flex items-center justify-between gap-2 bg-black/80 text-white rounded-full mx-auto hover:bg-black duration-500'
+              >Submit now <Image src={assets.right_arrow_white} alt='' className='w-4'/></button>
+
+              <p className='mt-4'>sending....</p>
+            </form>
     </div>
   )
 }
