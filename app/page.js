69cc6267012835588c8a28649bcb042c8835f625
okurutjoseph <okurutjoseph97@gmail.com>
2025-02-20 11:01:28 +0300

    header section added

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 'use Client'
+import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <main>
       <Navbar />
+      <Header />
     </main>
   );
 }
