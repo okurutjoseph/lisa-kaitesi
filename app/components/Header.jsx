3fe4a2c26f0dceee79435cc9a3dfecaf010cd400
okurutjoseph <okurutjoseph97@gmail.com>
2025-02-21 10:25:20 +0300

    header edited

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
       <h3 className='flex items-end gap-2 text-xl md:text-2xl mb-3 font-Ovo'>
             Hi! I'm Lisa Kaitesi <Image src={assets.hand_icon} alt='' className='w-6'/></h3>
         <h1 className='text-3xl sm:text-6xl lg:text-[66px] font-Ovo'>
-            frontned web developer based in London.</h1>
+            frontend web developer based in Uganda.</h1>
         <p className='max-w-2xl mx-auto font-Ovo'>
             I am a frontend developer from California, USA with 10 years of experience in multiple companies like Microsoft, Tesla and Apple.
         </p>
